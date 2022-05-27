e71127f1cf9da0daf883cbe9c0dc92d12a2149cf
Jon Moore <jonevanmoore@yahoo.com>
2022-05-27 15:58:28 -0500

    onclick about me

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const jonLink = document.getElementById('jon-link')
-    jonLink.style.color = 'pink'
+
+    const aboutMe = document.getElementById('about-me')
+    aboutMe.style.display = 'none'
+
+    const aboutMeLink = document.getElementById('about-me-link')
+    aboutMeLink.addEventListener('click', e => {
+        if (aboutMe.style.display === 'none') {
+            aboutMe.style.display = 'block'
+        } else {
+            aboutMe.style.display = 'none'
+        }
+    })
 })
