7dd07da1b6dfd4b6e80e0c70000f2f28f2317eb9
Jon Moore <jonevanmoore@yahoo.com>
2022-09-08 11:08:55 -0700

    new look

--- javascript.js
+++ javascript.js
@@ -1,34 +1,66 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //PHOTONEST
+    let slideIndex = 0;
+    showSlides();
 
-    const aboutMe = document.getElementById('about-me')
-    const myProjects = document.getElementById('project')
-    aboutMe.style.display = 'block'
-    myProjects.style.display = 'none'
-
-    const aboutMeLink = document.getElementById('about-me-link')
-    aboutMeLink.style.color = 'rgb(107, 107, 255)'
-    const projectLink = document.getElementById('project-link')
-    aboutMeLink.addEventListener('click', e => {
-        if (aboutMe.style.display === 'none') {
-            aboutMe.style.display = 'block'
-            aboutMeLink.style.color = 'rgb(107, 107, 255)'
-            myProjects.style.display = 'none'
-            projectLink.style.color = 'black'
-        } else {
-            aboutMe.style.display = 'none'
-            aboutMeLink.style.color = 'black'
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
         }
-    })
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 4000); // Change image every 4 seconds
+    }
 
-    projectLink.addEventListener('click', e => {
-        if (myProjects.style.display === 'none') {
-            myProjects.style.display = 'block'
-            projectLink.style.color = 'rgb(107, 107, 255)'
-            aboutMe.style.display = 'none'
-            aboutMeLink.style.color = 'black'
-        } else {
-            myProjects.style.display = 'none'
-            projectLink.style.color = 'black'
+
+    // JELLO
+    let slideIndex1 = 0;
+    showSlides1();
+
+    function showSlides1() {
+        let i;
+        let slides1 = document.getElementsByClassName("mySlides1");
+        for (i = 0; i < slides1.length; i++) {
+            slides1[i].style.display = "none";
         }
-    })
+        slideIndex1++;
+        if (slideIndex1 > slides1.length) { slideIndex1 = 1 }
+        slides1[slideIndex1 - 1].style.display = "block";
+        setTimeout(showSlides1, 4000); // Change image every 4 seconds
+    }
+
+    // QUILLED
+    let slideIndex2 = 0;
+    showSlides2();
+
+    function showSlides2() {
+        let i;
+        let slides2 = document.getElementsByClassName("mySlides2");
+        for (i = 0; i < slides2.length; i++) {
+            slides2[i].style.display = "none";
+        }
+        slideIndex2++;
+        if (slideIndex2 > slides2.length) { slideIndex2 = 1 }
+        slides2[slideIndex2 - 1].style.display = "block";
+        setTimeout(showSlides2, 4000); // Change image every 4 seconds
+    }
+
+    // SNACK
+    let slideIndex3 = 0;
+    showSlides3();
+
+    function showSlides3() {
+        let i;
+        let slides3 = document.getElementsByClassName("mySlides3");
+        for (i = 0; i < slides3.length; i++) {
+            slides3[i].style.display = "none";
+        }
+        slideIndex3++;
+        if (slideIndex3 > slides3.length) { slideIndex3 = 1 }
+        slides3[slideIndex3 - 1].style.display = "block";
+        setTimeout(showSlides3, 4000); // Change image every 4 seconds
+    }
 })
